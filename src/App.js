90c3e2a760b33290e26b0ca9743dfecc49119182
Sukhman Singh <ssingh8_be20@thapar.edu>
2023-07-05 01:21:20 +0530

    react components and props

--- src/App.js
+++ src/App.js
@@ -1,12 +1,52 @@
-
 import './App.css';
 
 function App() {
+  // const name = <h1>Sukhman</h1>;
+  // const age = <h2>21</h2>
+  // const email = <h2>sample@email.com</h2>
+  // const user = (
+  //   <div>
+  //     {name}
+  //     {age}
+  //     {email}
+  //   </div>
+  // );
   return (
     <div className="App">
-      
+      {/* {name} */}
+      {/* {name} */}
+      < User name='Sukhman' age = {20} email="sample@email.com" />
+      < User name='Singh' age = {20} email="sample@email.com" />
     </div>
   );
 }
+
+
+const User = (props) => {
+  return (
+  <div>
+    <h1>{props.name}</h1>
+    <h2>{props.age}</h2>
+    <h2>{props.email}</h2>
+  </div>)
+}
+
+
+// const User = () => {
+//   return (
+//   <div>
+//     <h1>Sukhman</h1>
+//     <h2>21</h2>
+//     <h2>sample@email.com</h2>
+//   </div>)
+// }
+
+// const GetName = () => {
+//   return 'Sukhman';
+// }
+
+// const GetNameComponent = () => {
+//   return <h1>Sukhman</h1>;
+// }
 
 export default App;
