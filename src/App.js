2559f4554d5d92e959bc1799e29bcf188ff6d16d
Sukhman Singh <ssingh8_be20@thapar.edu>
2023-07-05 01:37:06 +0530

    Ternary operators and conditionals in react

--- src/App.js
+++ src/App.js
@@ -1,52 +1,18 @@
 import './App.css';
 
+
+
 function App() {
-  // const name = <h1>Sukhman</h1>;
-  // const age = <h2>21</h2>
-  // const email = <h2>sample@email.com</h2>
-  // const user = (
-  //   <div>
-  //     {name}
-  //     {age}
-  //     {email}
-  //   </div>
-  // );
-  return (
-    <div className="App">
-      {/* {name} */}
-      {/* {name} */}
-      < User name='Sukhman' age = {20} email="sample@email.com" />
-      < User name='Singh' age = {20} email="sample@email.com" />
+  const age = 15;
+  const isGreen = true;
+
+  return(
+    <div>
+      {age >= 18 ? <h1>OVERAGE</h1> : <h1>UNDERAGE</h1>}
+      <h1 style={{color : isGreen ? "green" : "red"}}>This has color</h1>
+      {isGreen && <button> this is a button</button>}
     </div>
   );
 }
-
-
-const User = (props) => {
-  return (
-  <div>
-    <h1>{props.name}</h1>
-    <h2>{props.age}</h2>
-    <h2>{props.email}</h2>
-  </div>)
-}
-
-
-// const User = () => {
-//   return (
-//   <div>
-//     <h1>Sukhman</h1>
-//     <h2>21</h2>
-//     <h2>sample@email.com</h2>
-//   </div>)
-// }
-
-// const GetName = () => {
-//   return 'Sukhman';
-// }
-
-// const GetNameComponent = () => {
-//   return <h1>Sukhman</h1>;
-// }
 
 export default App;
